e31ac7c5baa44265ad928abbcb53828d8ef6b0b6
vinicio <vinicio.borja10@gmail.com>
2025-01-14 06:49:55 -0500

    Add function to validate digital clock times in 24-hour format

---
+++ generateValidTimes.js
@@ -0,0 +1,27 @@
+/*
+    Retos propuestos:
+
+    Reloj digital:
+    Genera todas las combinaciones posibles de un reloj digital de 24 horas con 4 dígitos. Por ejemplo:
+
+    12:34 es válido.
+
+    25:61 no es válido.
+    El objetivo es que vuestro programa imprima o devuelva todas las combinaciones válidas. ⏰
+*/
+
+/**
+ * @param {string[]} text
+ * @return {boolean}
+ */
+const generateValidTimes = (timeString = "") => {
+  const [hours, minutes] = timeString.split(":");
+  if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
+    return `${timeString} es válido.`;
+  }
+
+  return `${timeString} no es válido.`;
+};
+
+console.log(generateValidTimes("12:34"));
+console.log(generateValidTimes("25:61"));
